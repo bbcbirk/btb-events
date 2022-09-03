07c56680d5403d67b67cfde99a1b3bc82d7ccf9c
Birk Thestrup Blauner <birkblauner@gmail.com>
2022-09-03 15:39:57 +0200

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a913a8f339f8922f4ab11a1059ed2e129d49ccf5',
+        'name' => 'wp/btb-events',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jjgrainger/posttypes' => array(
+            'pretty_version' => 'v2.2',
+            'version' => '2.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jjgrainger/posttypes',
+            'aliases' => array(),
+            'reference' => '20f5a203316783a71ddfcd047ffebf1dc2bacb51',
+            'dev_requirement' => false,
+        ),
+        'wp/btb-events' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a913a8f339f8922f4ab11a1059ed2e129d49ccf5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
