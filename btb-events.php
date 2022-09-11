61b503ff70d2a65b1c036861f58c39b9196df228
Birk Thestrup Blauner <birkblauner@gmail.com>
2022-09-11 17:36:16 +0200

    Version

--- btb-events.php
+++ btb-events.php
@@ -4,7 +4,7 @@
  * Description:       Example static block scaffolded with Create Block tool.
  * Requires at least: 5.9
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           v1.0.3
  * Author:            Birk Thestrup Blauner
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
